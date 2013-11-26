8f267cf49428b2a6cd6cd005349f5b28427bc1ab
danjac8 <djacobs8@gmu.edu>
2013-11-26 17:08:09 -0500

    Got high five working
    check voice recognition to make sure I didn't accidentally break it

--- SOMI_Says_GameShell.py
+++ SOMI_Says_GameShell.py
@@ -19,70 +19,70 @@ SendInput = ctypes.windll.user32.SendInput
 
 PUL = ctypes.POINTER(ctypes.c_ulong)
 class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+	_fields_ = [("wVk", ctypes.c_ushort),
+				("wScan", ctypes.c_ushort),
+				("dwFlags", ctypes.c_ulong),
+				("time", ctypes.c_ulong),
+				("dwExtraInfo", PUL)]
 
 class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
+	_fields_ = [("uMsg", ctypes.c_ulong),
+				("wParamL", ctypes.c_short),
+				("wParamH", ctypes.c_ushort)]
 
 class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+	_fields_ = [("dx", ctypes.c_long),
+				("dy", ctypes.c_long),
+				("mouseData", ctypes.c_ulong),
+				("dwFlags", ctypes.c_ulong),
+				("time",ctypes.c_ulong),
+				("dwExtraInfo", PUL)]
 
 class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
+	_fields_ = [("ki", KeyBdInput),
+				 ("mi", MouseInput),
+				 ("hi", HardwareInput)]
 
 class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
+	_fields_ = [("type", ctypes.c_ulong),
+				("ii", Input_I)]
 
 def PressKey(hexKeyCode):
 
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( hexKeyCode, 0x48, 0, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+	extra = ctypes.c_ulong(0)
+	ii_ = Input_I()
+	ii_.ki = KeyBdInput( hexKeyCode, 0x48, 0, 0, ctypes.pointer(extra) )
+	x = Input( ctypes.c_ulong(1), ii_ )
+	ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 def ReleaseKey(hexKeyCode):
 
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( hexKeyCode, 0x48, 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-    
+	extra = ctypes.c_ulong(0)
+	ii_ = Input_I()
+	ii_.ki = KeyBdInput( hexKeyCode, 0x48, 0x0002, 0, ctypes.pointer(extra) )
+	x = Input( ctypes.c_ulong(1), ii_ )
+	ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+	
 def save():
 
-    PressKey(0x11) #Alt
-    PressKey(0x53) #Tab
+	PressKey(0x11) #Alt
+	PressKey(0x53) #Tab
 
-    #optional : if you want to see the atl-tab overlay
+	#optional : if you want to see the atl-tab overlay
 
-    ReleaseKey(0x11) #~Tab
-    ReleaseKey(0x53) #~Alt
+	ReleaseKey(0x11) #~Tab
+	ReleaseKey(0x53) #~Alt
 
 def clear():
-    PressKey(0x11)
-    PressKey(0x41)
-    
+	PressKey(0x11)
+	PressKey(0x41)
+	
    
-    
-    ReleaseKey(0x11)#ctrl
-    ReleaseKey(0x41)#a
-    
-    PressKey(0x08)#backspace
+	
+	ReleaseKey(0x11)#ctrl
+	ReleaseKey(0x41)#a
+	
+	PressKey(0x08)#backspace
 
 def MicOn():
 	PressKey(0x24)
@@ -102,9 +102,9 @@ def playMusic(mus_str):
 	p = pyaudio.PyAudio()  
 	#open stream  
 	stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-	                channels = f.getnchannels(),  
-	                rate = f.getframerate(),  
-	                output = True)  
+					channels = f.getnchannels(),  
+					rate = f.getframerate(),  
+					output = True)  
 	#read data  
 	data = f.readframes(chunk)  
 	
@@ -122,9 +122,9 @@ def playMusic(mus_str):
 
 def welcome():
 	os.chdir(r"C:\Program Files (x86)\Nuance\NaturallySpeaking12\Program")
-    os.startfile("natspeak.exe")
-    time.sleep(35)
-    MicOn()
+	os.startfile("natspeak.exe")
+	time.sleep(35)
+	MicOn()
 	print "Welcome to SOMI Says!"
 	r.turn(45,100)
 	time.sleep(0.2)
@@ -143,7 +143,7 @@ def success():
 def failure():
 	playMusic("EpicFailVoice.wav")
 	print "You have displeased the machine spirit and forfeited your immortal soul to the darkness!"
-	
+
 def waiting():
 	r.go(0,-20)
 	print "I'm waiting"
@@ -163,11 +163,8 @@ def proceed():
 	time.sleep(1)
 	file = open(path_to_file)
 	for line in file:
-   		if(line=="yes" or line=="Yes"):
-   			return True
-   		else:
-    		return False
-    #OLD VERSION
+		return (line=="yes" or line=="Yes")
+	#OLD VERSION
 	#response = ""
 	#while True:
 	#	response = raw_input("Do you want to play again? ")
@@ -179,68 +176,93 @@ def proceed():
 
 def whatAnimalMakesThisNoise():
 	path_to_file = "guesstheanimal.txt"
-    subprocess.Popen(["notepad.exe", path_to_file])
-    time.sleep(1)
-    clear()
-    #print("What is 2 + 2? ")
-    time.sleep(10)
-    save()
-    time.sleep(1)
-    os.system("TASKKILL /F /IM notepad.exe")
-    time.sleep(1)
-    file = open(path_to_file)
-    for line in file:
-    	if(line=="4"):
-    		return True
-    	if(line=="for"):
-    		return True
-    	if(line=="four"):
-    		return True
-    	if(line=="Four"):
-    		return True
-    	if(line=="For"):
-    		return True
-    	else:
-    		return False
-		
+	subprocess.Popen(["notepad.exe", path_to_file])
+	time.sleep(1)
+	clear()
+	time.sleep(10)
+	save()
+	time.sleep(1)
+	os.system("TASKKILL /F /IM notepad.exe")
+	time.sleep(1)
+	file = open(path_to_file)
+	for line in file:
+		if(line=="4"):
+			return True
+		if(line=="for"):
+			return True
+		if(line=="four"):
+			return True
+		if(line=="Four"):
+			return True
+		if(line=="For"):
+			return True
+		else:
+			return False
+
 def whatDoesThisAnimalSoundLike():
 	path_to_file = "makeanimalsounds.txt"
-    subprocess.Popen(["notepad.exe", path_to_file])
-    time.sleep(1)
-    clear()
-    #print("What is 2 + 2? ")
-    time.sleep(10)
-    save()
-    time.sleep(1)
-    os.system("TASKKILL /F /IM notepad.exe")
-    time.sleep(1)
-    file = open(path_to_file)
-    for line in file:
-    	if(line=="4"):
-    		return True
-    	if(line=="for"):
-    		return True
-    	if(line=="four"):
-    		return True
-    	if(line=="Four"):
-    		return True
-    	if(line=="For"):
-    		return True
-    	else:
-    		return False
+	subprocess.Popen(["notepad.exe", path_to_file])
+	time.sleep(1)
+	clear()
+	time.sleep(10)
+	save()
+	time.sleep(1)
+	os.system("TASKKILL /F /IM notepad.exe")
+	time.sleep(1)
+	file = open(path_to_file)
+	for line in file:
+		if(line=="4"):
+			return True
+		if(line=="for"):
+			return True
+		if(line=="four"):
+			return True
+		if(line=="Four"):
+			return True
+		if(line=="For"):
+			return True
+		else:
+			return False
 
 	
 def highFive():
-	print("Bump my nose!")
-	timeout = 0
+	# left bumper is right hand and vice versa
+	part = random.choice(["left hand","nose","right hand"])
+	print("Bump my %s!" % part)
+	timeout = time.clock() + 10
+	# until the timeout is reached, the program will check the sensors. If either is pressed,
+	# triggered will become True and the program will check 0.2 seconds later to make sure
+	# that the sensors are fully pressed (i.e. if I try to press the nose and the left hand
+	# happens to get pressed a millisecond before the right hand does, it shouldn't matter)
+	triggered = False
+	tries_left = 3
 	while True:
-		sensors = r.sensors([create.RIGHT_BUMP, create.LEFT_BUMP])
-		if (sensors[create.RIGHT_BUMP] == 1 or sensors[create.LEFT_BUMP] == 1):
-			return True
-		time.sleep(0.5)
-		timeout += 1
-		if (timeout == 20):
+		if (time.clock() > timeout or tries_left == 0):
 			return False
+		sensors = r.sensors([create.RIGHT_BUMP, create.LEFT_BUMP])
+		if (not triggered):
+			if (sensors[create.RIGHT_BUMP] == 1 or sensors[create.LEFT_BUMP] == 1):
+				# Sensors triggered - give the user 0.2 seconds to finish pressing down, then check again.
+				triggered = True
+				time.sleep(0.2)
+			else:
+				# Nothing happened - wait 10 milliseconds then check again.
+				time.sleep(0.01)
+		else:
+			if (not sensors[create.RIGHT_BUMP] == 1 and not sensors[create.LEFT_BUMP] == 1):
+				# Sensors were triggered 0.2 seconds ago but no longer register, must've been a fluke
+				triggered = False
+			elif ((part == "left hand" and sensors[create.RIGHT_BUMP] == 1 and sensors[create.LEFT_BUMP] == 0) or
+				(part == "right hand" and sensors[create.RIGHT_BUMP] == 0 and sensors[create.LEFT_BUMP] == 1) or
+				(part == "nose" and sensors[create.RIGHT_BUMP] == 1 and sensors[create.LEFT_BUMP] == 1)):
+				# Correct sensors pressed! Good job!
+				return True
+			else:
+				# Wrong sensors pressed! Try again
+				print("Sorry, you didn't press my %s! Please try again!" % part)
+				tries_left -= 1
+				time.sleep(0.5)
+				timeout = time.clock() + 10
 
 def catchMe():
 	time.sleep(2)
@@ -270,9 +292,9 @@ def catchMe():
 				else:
 					# turn randomly, pause, and shoot off in a different direction with a new timer
 					if (random.random() < 0.5):
-                                                r.turn(int(random.random() * 180) , 40)
-                                        else:
-                                                r.turn(-int(random.random() * 180) , -40)
+						r.turn(int(random.random() * 180) , 40)
+					else:
+						r.turn(-int(random.random() * 180) , -40)
 					time.sleep(2)
 					timeout = time.clock() + 6
 			#or if somi has timed out (lose condition)
@@ -311,5 +333,5 @@ def main():
 			break
 	
 r = create.Create(3)
-commands = [catchMe]
+commands = [highFive]
 main()
